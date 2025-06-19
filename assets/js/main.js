9bb80ab8a62500c945061e4b82c6489c9238f54f
hucallaway <80714875+hucallaway@users.noreply.github.com>
2025-06-19 17:25:37 -0400

    Remove title bar and all mobile header elements completely

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,18 +5,11 @@
 */
 
 (function($) {
-
-	var $window = $(window),
-		$body = $('body');
-
-	// Breakpoints.
-		breakpoints({
-			xlarge:  [ '1281px',  '1680px' ],
-			large:   [ '981px',   '1280px' ],
-			medium:  [ '737px',   '980px'  ],
-			small:   [ null,      '736px'  ]
-		});
-
-	// Maintenance mode - no navigation needed
-
+	// Maintenance mode - minimal JS needed
+	breakpoints({
+		xlarge:  [ '1281px',  '1680px' ],
+		large:   [ '981px',   '1280px' ],
+		medium:  [ '737px',   '980px'  ],
+		small:   [ null,      '736px'  ]
+	});
 })(jQuery);
